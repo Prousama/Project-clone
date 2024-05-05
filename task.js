e835895f7da4c8ab8a8f1149c234010b47f9b7eb
Prousama <167557160+Prousama@users.noreply.github.com>
2024-05-05 15:33:33 +0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,23 @@
+//Changing colour of button
+const button = document.getElementById('cc');
+button.onmouseup = ()=> {
+    button.style.backgroundColor = 'yellow';
+} 
+// button.onmouseenter = ()=> {
+//     button.style.backgroundColor = "yellow"
+// }
+
+//Color picker
+document.getElementById('apply-color').addEventListener('click', ()=> {
+    let color = document.getElementById('color-input').value;
+    document.getElementById('element-to-color').style.backgroundColor = color;
+  });
+  
+//Image enlarge button
+document.getElementById('enlarge-button').addEventListener('click', function() {
+    let image = document.getElementById('image');
+    let currentWidth = image.offsetWidth;
+    let newWidth = currentWidth * 1.2; // Increase the width by 20%
+    image.style.width = newWidth + 'px';
+  });
+  
